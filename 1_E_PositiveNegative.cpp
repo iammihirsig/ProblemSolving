73ef817e138a2bdaf062fd6cf84774dd21b57bf4
Mihir-Raj-Singh <mihirrajsingh123@gmail.com>
2025-02-09 12:09:02 +0530

    [Done] : 1_E_PositiveNegative

---
+++ 1_E_PositiveNegative.cpp
@@ -0,0 +1,35 @@
+/*
+Goal: To check whether the input no is +ve or -ve.
+NOTE: Not considering the 0 case.
+
+TC: O(1)
+SC: O(1)
+*/
+
+#include <iostream>
+using namespace std;
+
+// function declaration
+bool isPositive(int num)
+{
+  return (num > 0);
+}
+
+int main()
+{
+  // take input num
+  int num;
+  cin >> num;
+
+  // call isPositive()
+  if (isPositive(num))
+  {
+    cout << "Positive." << endl;
+  }
+  else
+  {
+    cout << "Negative." << endl;
+  }
+
+  return 0;
+}
