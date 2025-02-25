af8bc455d26ed18267d524cb13545ed409149a18
Mihir-Raj-Singh <mihirrajsingh123@gmail.com>
2025-02-25 13:17:23 +0530

    [Done]: 34_E_DecimalToOctal

---
+++ ProblemList/34_E_DecimalToOctal.cpp
@@ -0,0 +1,18 @@
+/*
+Goal: Convert Input Decimal No To Octal
+NOTE:
+
+TC: O(1)
+SC: O(1)
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int decimal;
+  cin >> decimal;
+
+  cout << "Octal: " << oct << decimal << endl; // Print in octal format
+  return 0;
+}
