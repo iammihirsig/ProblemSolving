3fff79b01d9f5d0e840e865a8716f618ba9d3c58
Mihir-Raj-Singh <mihirrajsingh123@gmail.com>
2025-04-26 21:37:13 +0530

    00_E_HelloWorld.cpp

---
+++ ProblemList/00_E_HelloWorld.cpp
@@ -0,0 +1,17 @@
+/*
+Author : Mihir Raj Singh
+Title : Hello World Code
+Date & Time : 2025-04-02 10:14:35
+*/
+
+#include <iostream>
+using namespace std;
+
+// Function definition and declaration starts
+
+// Function definition and declaration ends
+
+int main() {
+  cout << "Hello World, From C++!";
+  return 0;
+}
