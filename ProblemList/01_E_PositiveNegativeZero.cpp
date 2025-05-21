a679685ca8f9a549134b3f1f83e0a93375b86fc3
Mihir-Raj-Singh <mihirrajsingh123@gmail.com>
2025-05-21 11:03:25 +0530

    01_E_PositiveNegativeZero.cpp

---
+++ ProblemList/01_E_PositiveNegativeZero.cpp
@@ -0,0 +1,25 @@
+/*
+Author : Mihir Raj Singh
+Title : Check if number is positive, negative or zero
+Date & Time : 2025-05-21 10:35:13
+*/
+
+#include <iostream>
+using namespace std;
+
+// printClassification prints the status received according to classifyNumber (Zero, Postive, Negative)
+void printClassification(int val)
+{
+  string output;
+  output = (val >= 1) ? "Positive" : ((val < 0) ? "Negative" : "Zero");
+  cout << "Input Number \"" << val << "\" is : " << output << endl;
+}
+
+int main()
+{
+  int inputNumber;
+  cout << "Enter a number: ";
+  cin >> inputNumber;
+  printClassification(inputNumber);
+  return 0;
+}
